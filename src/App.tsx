ae86b1e006a730f1eb62b826b5af3bb9fd54be2a
Joel Semeniuk <joel.semeniuk@gmail.com>
2024-07-20 14:45:05 -0700

    Setting up styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-import './App.css'
-
 function App() {
   return (
-    <>
-      BUF!!
-    </>
+    <div className="bg-accent text-primary-content">
+      <div className="text-2xl pt-16 mx-9 text-center font-bold">
+        Be Undeniable Fitness
+      </div>
+      <div className="text-xl mb-16 mx-9 text-center">
+        Sub header text
+      </div>
+      <div className="bg-neutral-content text-primary-content grid place-content-center rounded-2xl mx-auto">
+        <div className="my-3 mx-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas pulvinar felis ac blandit efficitur. Donec aliquam pharetra ante, sed commodo arcu tristique eu. Suspendisse id sagittis odio. Sed non lectus molestie, egestas lectus ornare, venenatis felis. Morbi feugiat tellus tortor, in iaculis dui rhoncus quis. Morbi arcu dolor, dapibus ac lobortis id, faucibus non nulla. Cras sed maximus orci. Nulla tortor ex, vehicula a velit vitae, ullamcorper congue augue. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus quam elit, accumsan vitae purus sit amet, euismod pretium sem. Nullam consequat diam bibendum est scelerisque, id maximus metus iaculis.
+
+Mauris efficitur tellus turpis, vitae tempor eros vehicula eu. Proin convallis justo odio, quis feugiat ex ornare non. Vestibulum id mattis urna. Aliquam nec mauris purus. In hac habitasse platea dictumst. Morbi at pulvinar turpis. Aenean volutpat, tellus vel molestie condimentum, sem eros luctus nibh, non finibus velit diam sed mauris. Morbi pharetra molestie quam, ac finibus risus ullamcorper non. Aliquam nec erat ut mi gravida vestibulum. Pellentesque porta maximus lorem. Fusce dignissim vehicula ultricies. Ut ultricies quis mi eu sodales. Duis consequat in odio ac viverra.
+
+Aenean finibus cursus dui eget posuere. Integer fringilla vitae urna ac imperdiet. Aliquam faucibus nunc id dolor mollis aliquam. Cras imperdiet felis sed interdum lacinia. Praesent laoreet metus vitae viverra gravida. Cras porta massa arcu, a congue tellus rhoncus vitae. Vivamus ac sagittis quam, sit amet placerat nulla. Vivamus sit amet elementum dui, vel placerat sapien. Aliquam vitae ultricies odio. Quisque urna enim, egestas finibus tempus nec, pretium quis purus. Morbi tincidunt, ante nec convallis aliquet, mauris erat suscipit massa, sed consectetur ipsum erat id turpis. Morbi molestie hendrerit tempus. Vestibulum aliquam posuere malesuada. Cras eget mattis tortor. Nam fringilla, quam sed vestibulum sodales, ante libero hendrerit ligula, vulputate cursus est orci eu nisl.
+
+Aliquam congue bibendum volutpat. Cras non egestas turpis. Donec eleifend sed metus auctor fermentum. Proin tempus a lacus at sagittis. Vivamus non congue nisl, ut bibendum est. In laoreet felis ante. In hac habitasse platea dictumst. Phasellus nisl est, fermentum et ornare ut, semper eget nisl. Phasellus venenatis sit amet enim sit amet euismod. Cras scelerisque urna id dignissim suscipit. Phasellus lorem nisi, laoreet sed eros dignissim, tincidunt hendrerit augue. Suspendisse ipsum quam, laoreet sed enim id, convallis luctus arcu. Morbi dui nisi, feugiat sit amet porttitor id, gravida ultricies neque. Donec vitae viverra purus.
+
+Cras ligula sapien, malesuada id viverra non, vehicula quis leo. Aenean hendrerit ipsum auctor est pulvinar, ac fermentum nulla aliquet. Sed rhoncus commodo metus quis fermentum. Etiam ullamcorper, tellus laoreet faucibus sollicitudin, quam lectus maximus odio, at condimentum urna urna at ligula. Nulla bibendum tellus ut enim laoreet semper. Nunc nec lectus vestibulum, iaculis felis id, accumsan leo. Mauris dictum quam pellentesque neque tincidunt, id scelerisque dui finibus. Nulla tempus auctor pharetra. Curabitur vitae leo vel eros convallis consequat a at tellus. Sed vitae ex tortor. Vivamus molestie volutpat enim, convallis blandit elit fringilla nec. Donec vehicula pellentesque urna eu varius. Ut vestibulum dignissim tellus ac blandit. Morbi eleifend lacinia enim quis egestas. Proin lacinia sem egestas nisi dignissim vestibulum.</div>
+      </div>
+    </div>
   )
 }
 
